5297f5ea51ab7e440722a753e049d4a4db06bd71
Dinesh katwal <dinesh.katwal@logicabeans.com>
2018-10-04 21:07:46 +0545

    Added bulk sms feature and also added banner.txt

--- src/main/java/com/example/kafka/IntegrationAPI.java
+++ src/main/java/com/example/kafka/IntegrationAPI.java
@@ -1,22 +1,39 @@
 package com.example.kafka;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IntegrationAPI {
 
     @Autowired
     private KafkaMailProducer kafkaMailProducer;
 
     @PostMapping("/sendEmail")
-    public ResponseEntity sendMessage(@RequestBody Request request) {
+    public ResponseEntity<String> sendMessage(@RequestBody Request request) {
         kafkaMailProducer.produceMail().accept(request);
         return new ResponseEntity<>("Message sent to " + request.toEmail, HttpStatus.OK);
+    }
+    /**
+     * 
+     * @param requests List of request to send an email.
+     * @return
+     */
+    @PostMapping("/bulkEmail")
+    public List<ResponseEntity<String>> sendMessage(@RequestBody List<Request> requests) {
+        List<ResponseEntity<String>> responses= new ArrayList<>();
+        requests.forEach((request)-> {
+            kafkaMailProducer.produceMail().accept(request);
+            responses.add(new ResponseEntity<>("Message sent to " + request.toEmail, HttpStatus.OK));
+        });
+        return responses;
     }
 
 }
